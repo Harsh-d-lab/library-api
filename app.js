a78b58798bed977902bdbeb0c0d8d4c5c85e6c13
Harsh Dhama <dhamaharsh9@gmail.com>
2024-08-05 22:16:55 +0530

    verifying app

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ mongoose.connect(config.mongoURI)
         console.error('Could not connect to MongoDB', err);
     });
 
-// Start the server
 app.listen(config.port, () => {
     console.log(`Server running on port ${config.port}`);
 });
